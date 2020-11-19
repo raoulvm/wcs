c6fd9533909e610b2762d6489c15d7f906ecc2cf
Raoul <snowdd1@users.noreply.github.com>
2020-11-19 15:48:06 +0100

    Bump version number

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wcs',
-    version='0.0.5',
+    version='0.0.6',
     description='wcs tools for colab and co',
     url='git@github.com/snowdd1/wcs.git',
     author='Raoul',
