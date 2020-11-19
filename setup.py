ff374dd676258e85f4577ac94f747e73b8c67f9a
Raoul <snowdd1@users.noreply.github.com>
2020-11-19 15:43:09 +0100

    setup: auto-package detection

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name='wcs',
@@ -8,6 +8,6 @@ setup(
     author='Raoul',
     author_email='snowdd1.theelk@xoxy.net',
     license='MIT2.0',
-    packages=['wcs', 'wcs.skl'],
+    packages=find_packages(), # Auto setup all packages
     zip_safe=False
 )
