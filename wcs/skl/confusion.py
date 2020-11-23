02458cde496de87665855bdd7a23e6f5a653c2a6
Raoul <snowdd1@users.noreply.github.com>
2020-11-23 15:15:19 +0100

    typo docstring

--- wcs/skl/confusion.py
+++ wcs/skl/confusion.py
@@ -4,8 +4,8 @@ from IPython.display import display
 from .metrics import pretty_confusionmatrix as new_location
 
 def pretty_confusionmatrix(*args, **kwargs)->None:
-    """Deprecation warning: wcs.skl.confusion.pretty_confusionmatrix() has been moved to wcs.skl.metrics.pretty_confusionmatrix() \nw Just to confuse you even more.
+    """Deprecation warning: wcs.skl.confusion.pretty_confusionmatrix() has been moved to wcs.skl.metrics.pretty_confusionmatrix() \n Just to confuse you even more.
 
     """
-    print('Deprecation warning: wcs.skl.confusion.pretty_confusionmatrix() has been moved to wcs.skl.metrics.pretty_confusionmatrix() \nw Just to confuse you even more.')
+    print('Deprecation warning: wcs.skl.confusion.pretty_confusionmatrix() has been moved to wcs.skl.metrics.pretty_confusionmatrix() \n Just to confuse you even more.')
     return new_location(*args, **kwargs)
