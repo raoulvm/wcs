8f5fba9526bc5baf82420e4022bfb6d1eb72b5ff
Raoul <snowdd1@users.noreply.github.com>
2020-11-25 10:38:08 +0100

    bump vernum

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wcs',
-    version='0.0.23',
+    version='0.0.24',
     description='wcs tools for colab and co',
     url='git@github.com/snowdd1/wcs.git',
     author='Raoul',
