356e309cb3298ca6c3f18aea176f4bcaaaa8bcd2
Raoul <snowdd1@users.noreply.github.com>
2021-01-06 16:17:08 +0100

    class Winsor_absolute()

--- wcs/skl/preprocessing.py
+++ wcs/skl/preprocessing.py
@@ -87,3 +87,72 @@ class Winsor(TransformerMixin):
     def __repr__(self):
         return f"Winsor(q_low_cut={self.q_low_cut}, q_high_cut={self.q_high_cut}, name={self.name}, verbose={self.verbose}) "
 
+class Winsor_absolute(TransformerMixin):
+    '''
+    Winsorization Transformer
+    Extreme values, defined by absolutes, will be set the to min max value 
+    specified.  
+    
+    Parameters:
+    -----------
+    low_cut = float  : Set the low cut margin individually, eg 0.02
+                            to cut the lowest 2% of the values,
+    high_cut = float : Set the high cut margin individually, eg 0.80
+                            to cut the top 20% of the values
+
+    '''
+    def __init__(self, 
+                       low_cut:float=None,
+                       high_cut:float=None,
+                       name:str="winsor_absolute",
+                       verbose:bool=False):
+
+        self.high_cut = high_cut
+        self.low_cut = low_cut
+        if self.high_cut<=self.low_cut :
+            raise ValueError(f'Values must be float and low_cut < high_cut, got {low_cut},{high_cut}')
+
+        self.name = name
+        self.verbose = verbose
+        
+    def __return_feature_names(self, s:List=None):
+        pname = self.name+'_' if self.name is not None else ''
+        if isinstance(s, list) and len(s)>0 and isinstance(s[0], str): 
+            return [s[0]+'__'+pname+c for c in self.__features]
+        else:
+            return [pname+c for c in self.__features]
+    def fit(self, X, y=None):
+        if self.verbose:
+            print(self.name, 'fit() called!')
+            print('X is ', type(X))
+        if isinstance(X, pd.core.frame.DataFrame):
+            self.__features = X.columns
+            self.get_feature_names = self.__return_feature_names
+        if len(X.shape)!=2:
+            raise ValueError('Data is not 2 dimensional. Either use np.reshape(-1,1) or double square brackets with pd.DataFrames!')
+
+        return self
+    def transform(self, X, y=None):
+        if self.verbose:
+            print(self.name,'transform called!')
+            print('X is ', type(X))
+        if len(X.shape)!=2:
+            raise ValueError('Data is not 2 dimensional. Either use np.reshape(-1,1) or double square brackets with pd.DataFrames!')
+
+        if isinstance(X, pd.core.frame.DataFrame):
+            X2 = X.to_numpy(copy=True)
+        else: 
+            X2 = X.copy()
+        for c in range(X2.shape[1]):
+            if self.low_cut is not None:
+                X2[:,c][X2[:,c]<self.low_cut]  = self.low_cut
+            if self.high_cut is not None:
+                X2[:,c][X2[:,c]>self.high_cut] = self.high_cut
+        return X2
+
+    def get_params(self, deep:bool=True):
+        # required to create unfitted clones
+        return {'low_cut':self.low_cut, 'high_cut':self.high_cut, 'name':self.name, 'verbose':self.verbose} # return all init-Paramters as dict
+
+    def __repr__(self):
+        return f"Winsor_absolute(low_cut={self.low_cut}, high_cut={self.high_cut}, name={self.name}, verbose={self.verbose}) "
