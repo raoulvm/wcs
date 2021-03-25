688562887db982deee215c1c78effa372d7211ea
Raoul <79988187+raoulvm@users.noreply.github.com>
2021-03-25 14:45:27 +0100

    bump vernum

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='wcs',
-    version='0.6.14',
+    version='0.6.15',
     description='wcs tools for colab and co',
     url='git@github.com/raoulvm/wcs.git',
     author='Raoul',
